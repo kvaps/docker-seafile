bf25e8511d0a3d4702ab94be17d6e837e9080c6c
kvaps <kvapss@gmail.com>
2015-12-10 17:26:31 +0300

    Add start.sh script

--- start.sh
+++ start.sh
@@ -1,87 +1,171 @@
 #!/bin/bash
 
-expect <<EOF
+random_pwd()
+{
+        cat /dev/urandom | env LC_CTYPE=C tr -dc a-zA-Z0-9 | head -c 16; echo;
+}
+
+generate_dn()
+{
+    echo $(hostname -d) | sed 's/^/dc=/g' | sed 's/[\.]/,dc=/g'
+}
+
+chk_var () {
+   var=$(sh -c "echo $(echo \$$1)")
+   [ -z "$var" ] && export "$1"="$2"
+}
+
+load_defaults()
+{
+    chk_var  TZ                    "utc"
+    chk_var  SERVER_NAME           "seafile"
+    chk_var  SERVER_HOSTNAME       "seafile"
+    chk_var  SEAFILE_DATA          "/data/seafile-data"
+    chk_var  SEAHUB_PORT           "8082"
+    chk_var  MYSQL_CREATE_DB       1
+#   chk_var  MYSQL_ROOT_PASSWD     ""
+    chk_var  MYSQL_HOSTNAME        "seafile-db"
+    chk_var  MYSQL_PORT            "3306"
+    chk_var  MYSQL_USER            "seafile"
+#    chk_var  MYSQL_PASSWD          ""
+    chk_var  MYSQL_CCNET_DBNAME    "ccnet-db"
+    chk_var  MYSQL_SEAFILE_DBNAME  "seafile-db"
+    chk_var  MYSQL_SEAHUB_DBNAME   "seahub-db"
+#    chk_var  ADMIN_EMAIL           "test@example.org"
+#    chk_var  ADMIN_PASSWD          `random_pwd`
+}
+
+set_timezone()
+{
+    if [ -f /usr/share/zoneinfo/$TZ ]; then 
+        rm -f /etc/localtime && ln -s /usr/share/zoneinfo/$TZ /etc/localtime
+    fi
+}
+
+move_dirs()
+{
+    echo "info:  start moving Seafile folders"
+    cp -rax /usr/src/seafile-server-*/ /data/seafile/
+    echo "info:  finished moving Seafile folders"
+}
+
+configure_seafile()
+{
+    echo "info:  start configuring Seafile"
+
+    [ -z "$ADMIN_EMAIL" ] && echo "err:   \$ADMIN_EMAIL is not set" ; exit 1
+    [ -z "$ADMIN_PASSWD" ] && echo "err:   \$ADMIN_PASSWD is not set" ; exit 1
+
+    IFS='' 
+    
+    first_step=$(cat<<EOF
 spawn   ./setup-seafile-mysql.sh
 set timeout 300
 expect  "Press ENTER to continue"
 send    "\r"
 expect  " server name "
-send    "test\r"
+send    "${SERVER_NAME}\r"
 expect  " This server's ip or domain "
-send    "0.0.0.0\r"
+send    "${SERVER_HOSTNAME}\r"
 expect  " default "
-send    "/data/seafile-data\r"
+send    "${SEAFILE_DATA}\r"
 expect  " default \"8082\" "
-send    "8082\r"
+send    "${SEAHUB_PORT}\r"
 expect  " 1 or 2 "
-send    "1\r"
+send    "${MYSQL_CREATE_DB}\r"
 expect  " default \"localhost\" "
-send    "seafile-db\r"
+send    "${MYSQL_HOSTNAME}\r"
 expect  " default \"3306\" "
-send    "3306\r"
+send    "${MYSQL_PORT}\r"
 expect  " mysql user for seafile "
-send    "seafile\r"
-expect  " password for * "
-send    "seafile\r"
-expect  " ccnet database "
-send    "seafile\r"
-expect  " seafile database "
-send    "seafile\r"
-expect  " seahub database "
-send    "seafile\r"
-expect  "Press ENTER to continue"
-send    "\r"
-expect  "Your seafile server configuration has been finished successfully."
-exit    0
 EOF
+    )
 
-expect <<EOF
-spawn   ./setup-seafile-mysql.sh
-set timeout 300
-expect  "Press ENTER to continue"
-send    "\r"
-expect  " server name "
-send    "test\r"
-expect  " This server's ip or domain "
-send    "0.0.0.0\r"
-expect  " default "
-send    "/data/seafile-data\r"
-expect  " default \"8082\" "
-send    "8082\r"
-expect  " 1 or 2 "
-send    "1\r"
-expect  " default \"localhost\" "
-send    "seafile-db\r"
-expect  " default \"3306\" "
-send    "3306\r"
+    if [ "$MYSQL_CREATE_DB" = "1" ]; then
+
+        [ -z "$MYSQL_ROOT_PASSWD" ] && echo "err:   \$MYSQL_ROOT_PASSWD is not set" ; exit 1
+        second_step=$(cat<<EOF
+
 expect  " root password "
-send    "seafile\r"
+send    "${MYSQL_ROOT_PASSWD}\r"
 expect  " default \"root\" "
-send    "root\r"
-expect  " default \"ccnet-db\" "
-send    "ccnet-db\r"
-expect  " default \"seafile-db\" "
-send    "seafile-db\r"
-expect  " default \"seahub-db\" "
-send    "seahub-db\r"
+send    "${MYSQL_USER}\r"
+EOF
+    )
+
+        if [ "$MYSQL_USER" != "root"]; then
+
+        [ -z "$MYSQL_PASSWD" ] && echo "err:   \$MYSQL_PASSWD is not set" ; exit 1
+            third_step=$(cat<<EOF
+
+expect  " password for * "
+send    "${MYSQL_PASSWD}\r"
+EOF
+            )
+        fi
+
+    elif [ "$MYSQL_CREATE_DB" = "2" ]; then
+
+        [ -z "$MYSQL_PASSWD" ] && echo "err:   \$MYSQL_PASSWD is not set" ; exit 1
+        second_step=$(cat<<EOF
+
+send    "${MYSQL_USER}\r"
+expect  " password for * "
+send    "${MYSQL_PASSWD}\r"
+expect  " ccnet database "
+EOF
+    )
+    fi
+
+    final_step=$(cat<<EOF
+
+send    "${MYSQL_CCNET_DBNAME}\r"
+expect  " seafile database "
+send    "${MYSQL_SEAFILE_DBNAME}\r"
+expect  " seahub database "
+send    "${MYSQL_SEAHUB_DBNAME}\r"
 expect  "Press ENTER to continue"
 send    "\r"
 expect  "Your seafile server configuration has been finished successfully."
 exit    0
 EOF
+    )
+    echo $first_step $second_step $third_step $final_step #| expect
 
-./seafile.sh start
+    unset IFS
+    ./seafile.sh start
 
-expect <<EOF
+    expect <<EOF
 spawn   ./seahub.sh start
 set timeout 300
 expect  " admin email "
-send    "test@test.com\r"
+send    "${ADMIN_EMAIL}\r"
 expect  " admin password "
-send    "test\r"
+send    "${ADMIN_PASSWD}\r"
 expect  " admin password again "
-send    "test\r"
+send    "${ADMIN_PASSWD}\r"
 expect  "Done."
 exit    0
 EOF
+    ./seafile.sh stop
+    ./seafile.sh stop
 
+
+    echo "info:  finished configuring Seafile"
+}
+
+start_services()
+{
+             echo "info:  Starting services"
+                      /usr/bin/supervisord
+} 
+
+[ ! -d /data/seafile ] && export FIRST_SETUP=true #Check for first setup
+
+                                load_defaults
+                                set_timezone
+if [ "$FIRST_SETUP" = true ] ; then
+                                move_dirs
+                                configure_seafile
+fi
+                                start_services
